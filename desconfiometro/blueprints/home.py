9388ea80ff1876b779b36c7402c4e75a30530f9d
Augusto Boranga <gutoboranga@gmail.com>
2019-11-09 14:40:23 -0300

    add api blueprint

--- desconfiometro/blueprints/home.py
+++ desconfiometro/blueprints/home.py
@@ -1,9 +1,8 @@
 from flask import redirect, url_for, render_template, Blueprint, request
 
 home_blueprint = Blueprint('home', __name__)
-indicators_count = 3
 
 @home_blueprint.route('/', methods = ['GET'])
 def index():
-    return render_template("index.html", indicators_count=indicators_count)
+    return render_template("index.html")
     
