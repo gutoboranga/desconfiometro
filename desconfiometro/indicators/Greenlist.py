f22cc808725451f9ef7da583aa46d25b6d475c2a
Augusto Boranga <gutoboranga@gmail.com>
2019-11-09 17:59:37 -0300

    fix christian's cagadas

--- desconfiometro/indicators/Greenlist.py
+++ desconfiometro/indicators/Greenlist.py
@@ -4,7 +4,7 @@ from desconfiometro.blueprints.models.Result import Result
 class Greenlist(BaseIndicator):
 
     def __init__(self):
-        self.file = open('desconfiometro/indicators/data/greenlist.txt')
+        self.filename = 'desconfiometro/indicators/data/greenlist.txt'
 
     def get_name(self):
         return "Lista dos confirmados"
@@ -19,12 +19,15 @@ class Greenlist(BaseIndicator):
         return self.is_greenlisted(parsed_url.netloc)
 
     def is_greenlisted(self, item):
-        line = self.file.readline()
         ok = False
         
-        while line:
-            if line.strip() == item:
-                ok = True
-            line = self.file.readline()
-        
+        with open(self.filename) as f:
+            line = f.readline()
+
+            while line:
+                if line.strip() == item:
+                    ok = True
+                    break
+                line = self.file.readline()
+                
         return Result(self.get_name(), self.get_description(), (1 if ok else 0), self.get_type())
